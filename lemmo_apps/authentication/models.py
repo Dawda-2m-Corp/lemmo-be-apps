b59fc5f7ef53c7b77ad61edbe51664adf6b8948f
Dawda Borje Kujabi <dawdaborjekujabi@gmail.com>
2025-07-23 21:27:58 +0000

    Updating....

--- lemmo_apps/authentication/models.py
+++ lemmo_apps/authentication/models.py
@@ -44,7 +44,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ["first_name", "last_name"]
 
     def __str__(self):
-        return f"{self.email} ({self.get_role_display()})"
+        return f"{self.email}"
 
     @property
     def full_name(self):
